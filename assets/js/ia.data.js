9e38d5a22ade24855ab4ee5ea86dfe013858f80a
조찬기 <36979670+isuda7@users.noreply.github.com>
2024-02-21 10:59:33 +0900

    IA 샘플링

--- assets/js/ia.data.js
+++ assets/js/ia.data.js
@@ -1,13 +1,75 @@
+const url_base = "../../html/";
 const ia = [
 	{
-		cateTitle: 'Pagelist',
+		cateTitle: 'Prototype',
 		cateItems: [
 			{
 				state: 'default', // default, done, hold, issue
-				url: '#',
-				menu: 'Depth1 > Depth2 > Content (Cases)',
+				url: '_prototype/layout.html',
+				menu: '레이아웃',
 				memos: [
-					{user: '작업자', date: 'YYYY-MM-DD', desc: '완료'},
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
+				],
+			},
+			{
+				state: 'default', // default, done, hold, issue
+				url: '_prototype/board_list.html',
+				menu: '목록화면',
+				memos: [
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
+				],
+			},
+			{
+				state: 'default', // default, done, hold, issue
+				url: '_prototype/board_form.html',
+				menu: '등록화면',
+				memos: [
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
+				],
+			},
+			{
+				state: 'default', // default, done, hold, issue
+				url: '_prototype/board_view.html',
+				menu: '상세화면',
+				memos: [
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
+				],
+			},
+		]
+	},
+	{
+		cateTitle: 'IA',
+		cateItems: [
+			{
+				state: 'default', // default, done, hold, issue
+				url: 'main/main.html',
+				menu: '메인',
+				memos: [
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
+				],
+			},
+			{
+				state: 'default', // default, done, hold, issue
+				url: 'main/searchBar.html',
+				menu: '메인 - 검색창',
+				memos: [
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
+				],
+			},
+			{
+				state: 'default', // default, done, hold, issue
+				url: 'about/history.html',
+				menu: 'About - 이순종 교수님 이력',
+				memos: [
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
+				],
+			},
+			{
+				state: 'default', // default, done, hold, issue
+				url: 'about/contact.html',
+				menu: 'About - 1:1문의',
+				memos: [
+					{user: '김도연', date: 'YYYY-MM-DD', desc: '완료'},
 				],
 			},
 		]
@@ -61,13 +123,13 @@ for (let i = 0; i < ia.length; i++){
 	ia_html = ia_html + ''
 	+'<div class="g-h2-head">'
 	+'	<h2 class="g-h2">'+ ia[i].cateTitle +' ('+ ia[i].cateItems.length +')</h2>'
-	+'	<span class="g-label-done">완료</span>'
-	+'	<span class="g-label-hold">보류</span>'
-	+'	<span class="g-label-issue">이슈</span>'
+	// +'	<span class="g-label-done">완료</span>'
+	// +'	<span class="g-label-hold">보류</span>'
+	// +'	<span class="g-label-issue">이슈</span>'
 
-	// +'	<button type="button" class="btn g-sec-toggle" onclick="guideSecToggle(this)">'
-	// +'		<span class="material-symbols-outlined">expand_circle_down</span>'
-	// +'	</button>'
+	+'	<button type="button" class="btn g-sec-toggle" onclick="guideSecToggle(this)">'
+	+'		<span class="material-symbols-outlined">expand_circle_down</span>'
+	+'	</button>'
 	+'</div>'
 	+'<div class="g-h2-body">'
 	+'	<ul class="g-ia-list">';
@@ -75,10 +137,10 @@ for (let i = 0; i < ia.length; i++){
 	for (let j = 0; j < ia[i].cateItems.length; j++){
 		ia_html = ia_html + ''
 		+'		<li class="g-ia-item is-'+ia[i].cateItems[j].state+'">'
-		+'			<a class="g-ia-primary" href="'+ia[i].cateItems[j].url+'">'
+		+'			<div class="g-ia-primary">'
 		+'				<div class="g-ia-num">'+(j+1)+'</div>'
 		+'				<div class="g-ia-menu">'
-		+'					<p>'+ ia[i].cateItems[j].menu +'</p>'
+		+'					<a href="'+url_base+ia[i].cateItems[j].url+'" target="_blank">'+ ia[i].cateItems[j].menu +'&nbsp;<span class="url">'+ia[i].cateItems[j].url+'</span></a>'
 		+'				</div>'
 		+'				<div class="g-ia-user" title="최근 작업자">'
 		// 작업내역 목록 - 작업자
